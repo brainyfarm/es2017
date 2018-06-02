1e14b4a8da175d4f79b2e27522129112f807b270
Olawale Akinseye <brainyfarm@gmail.com>
2018-06-02 12:29:52 +0100

    cathc async await errors

--- features/async.await/index.js
+++ features/async.await/index.js
@@ -10,6 +10,9 @@ import logger from 'util';
   */
  const greetAfterFiveSeconds = name => {
   return new Promise((resolve, reject) => {
+    if(name === 'Chike') {
+      reject("I cannot greet you Chike")
+    }
     setTimeout(() => { 
       resolve(`Hi there ${name}`);
      }, 5000);
@@ -24,8 +27,25 @@ import logger from 'util';
   * My log will only work when the function is resolved.
   */
  const greetMe = async name => {
-   const greetings = await greetAfterFiveSeconds('Olawale');
+   const greetings = await greetAfterFiveSeconds(name);
     logger.log(greetings);
 }
 
-greetMe(); // Hi there Olawale
+greetMe("Olawale"); // Hi there Olawale
+
+/**
+ * catching Errors with async await
+ * use try-catch
+ */
+
+const greetMeError = async name => {
+  try {
+    const greetings = await greetAfterFiveSeconds("Chike");
+    logger.log(greetings);
+  } catch(error){
+    logger.log(error);
+  }
+}
+
+
+greetMeError('Chike');
