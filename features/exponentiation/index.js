3d0bc3b7b48da813bebe067385a07ab653617137
Olawale Akinseye <brainyfarm@gmail.com>
2018-06-02 11:12:16 +0100

    add ** exponentiation

---
+++ features/exponentiation/index.js
@@ -0,0 +1,13 @@
+import logger from 'util';
+
+/**
+ * exponentiation
+ * an cleaner alternative to Math.pow
+ */
+
+ const twoExponentialTwoPow = Math.pow(2, 2); // 4
+
+const twoExponentialTwo = 2 ** 2; // 4
+
+logger.log(`Math.pow(2, 2): `, Math.pow(2, 2));
+logger.log(`2 ** 2: `, 2 ** 2);
